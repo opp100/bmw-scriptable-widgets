bf54068e7286e5d7bea87945a254d973d350c294
Youke XIANG <opp100@qq.com>
2021-11-02 12:09:52 +0800

    Chore: updated installer

--- Publish/installer.js
+++ Publish/installer.js
@@ -1,10 +1,17 @@
 const FILE_MGR = FileManager[module.filename.includes('Documents/iCloud~') ? 'iCloud' : 'local']();
+let files = FILE_MGR.listContents(FILE_MGR.documentsDirectory());
+
 await Promise.all(
     ['bmw-linker.js'].map(async (js) => {
         const REQ = new Request(`https://bmw-linker.yocky.info/${encodeURIComponent(js)}`);
         const RES = await REQ.load();
+        try {
+            FILE_MGR.remove(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js));
+        } catch (e) {}
+
         FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js), RES);
     })
 );
+
 FILE_MGR.remove(module.filename);
-// Safari.open('scriptable:///open?scriptName=' + encodeURIComponent('bmw-linker.js'));
+Safari.open('scriptable:///open?scriptName=');
