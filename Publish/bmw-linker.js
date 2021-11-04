d43425eeb4e31dc0e6f6fcba9d59f76aad11f8b8
Youke XIANG <opp100@qq.com>
2021-11-05 06:14:56 +0800

    Chore: updated CDN always pick the latest one

--- Publish/bmw-linker.js
+++ Publish/bmw-linker.js
@@ -758,8 +758,8 @@ let DEPENDENCIES = [
 let WIDGET_FONT = 'SF UI Display';
 let WIDGET_FONT_BOLD = 'SF UI Display Bold';
 let BMW_SERVER_HOST = 'https://myprofile.bmw.com.cn';
-let APP_HOST_SERVER = 'https://cdn.jsdelivr.net/gh/opp100/bmw-scriptable-widgets@main/Publish';
-let JS_CDN_SERVER = 'https://cdn.jsdelivr.net/gh/opp100/bmw-scriptable-widgets@main/lib';
+let APP_HOST_SERVER = 'https://cdn.jsdelivr.net/gh/opp100/bmw-scriptable-widgets/Publish';
+let JS_CDN_SERVER = 'https://cdn.jsdelivr.net/gh/opp100/bmw-scriptable-widgets/lib';
 
 let DEFAULT_BG_COLOR_LIGHT = '#FFFFFF';
 let DEFAULT_BG_COLOR_DARK = '#2B2B2B';
